a6aa685e1f8b6c7b6c4882e9d1125619c73f090f
Ryser <rysertom@gmail.com>
2018-12-18 16:11:26 +0100

    18.12.2018

--- ODT/magicColor/imagemagick.sh
+++ ODT/magicColor/imagemagick.sh
@@ -7,9 +7,17 @@
 
 shopt -s -o nounset
 
-declare MAX_PERCENT=100
 declare AXE_X=X
 declare AXE_Y=Y
+#start of headings declaration
+declare N="North"
+declare E="East"
+declare S="South"
+declare W="West"
+#end
+
+
+declare MAX_PERCENT=100
 declare COLOR=red
 declare SIZE=20
 declare DENSITY=90
@@ -21,58 +29,54 @@ declare TEXT
 declare POSITION_IN_PERCENT_X=1
 declare POSITION_IN_PERCENT_Y=1
 
-
-declare SYNTAX="Please use this syntax : /tag_images.sh {target folder} {text to add in image}"
+ 
+declare SYNTAX="Please use this syntax : /tag_images.sh {target folder} {text to add in image} {Position in heading [NW]}"
 
 function MODIFY_IMAGE {
-    #using gravity to choose where we are
-    #convert $1 -pointsize $SIZE -gravity NorthWest -fill $COLOR -annotate +30+10 "$TEXT" $target/$1
+    declare image="$1"
+    convert $image -pointsize $SIZE -gravity North -fill $COLOR -annotate +10+10 "$TEXT" $target/$image
+    convert $target/$image -pointsize $SIZE -gravity SouthEast -annotate +0+0 "$DATETIME" $target/$image
+
+}
+
+function HEADING_CONVERTOR {
+    local PARAM=$1
+    local PARAM1
+    local PARAM2
+
     
-    #using pixels to choose the position of the text
-    convert $1 -density $DENSITY -pointsize $SIZE  -fill $COLOR -annotate +$POSITION_TEXT_X+$POSITION_TEXT_Y "$TEXT" $target/$1
-    convert $target/$1 -pointsize $SIZE -gravity SouthWest -annotate +30+10 "$DATETIME" $target/$1
+    declare i=$((${#PARAM}))
+    PARAM1="${PARAM:0:1}"
+    PARAM2="${PARAM:1:1}"
 
+    case "" in
+    "1")
+        do_this()
+    ;;
+    "2" | "3")
+        do_what_you_are_supposed_to_do()
+    ;;
+    *)
+        do_nothing()
+    ;;
+    esac
 }
 
-function CALCULATE_POSITION_FROM_PERCENT {
-    local POSITION=$1
-    local SIZE_IMG=$2
-    local DIRECTION=$3
-    local MID_OF_MAX_PERCENT
-    let "MID_OF_MAX_PERCENT = ($MAX_PERCENT/2)"
-
-
-    if [[ "$DIRECTION" = "$AXE_X" && $POSITION -gt $MID_OF_MAX_PERCENT ]] 
-    then
-        let "SIZE_IMG = ($SIZE_IMG - $MARGIN_WIDTH)"
-    else
-        let "SIZE_IMG = ($SIZE_IMG + $MARGIN_WIDTH)"
-    fi
-
-    let "POSITION = (($POSITION * $SIZE_IMG) / $MAX_PERCENT)"
-    echo "$POSITION"
-}
-
-if [[ $# -lt 2 ]]; then
+if [[ $# -ne 3 ]]; then
     echo $SYNTAX
     exit -1
 else
     target=$1
     TEXT=$2
+    HEADING_CONVERTOR $3
     MARGIN_WIDTH=${#TEXT}
 
     [[ -d $target ]] || mkdir $target
 
     for file in *.jpg; 
     do 
-
-        declare IMAGE_HEIGHT=`convert $file -format '%h' info:` 
-        declare IMAGE_WIDTH=`convert $file -format '%w' info:`
-
-        POSITION_TEXT_X=$(CALCULATE_POSITION_FROM_PERCENT $POSITION_IN_PERCENT_X $IMAGE_WIDTH "$AXE_X")
-        POSITION_TEXT_Y=$(CALCULATE_POSITION_FROM_PERCENT $POSITION_IN_PERCENT_Y $IMAGE_HEIGHT "$AXE_Y")
         MODIFY_IMAGE $file
-        echo "$file done : $POSITION_TEXT_X, $POSITION_TEXT_Y image x $IMAGE_WIDTH, y $IMAGE_HEIGHT"
+        echo "$file done : position : $HEADING"
     done
 fi
 
